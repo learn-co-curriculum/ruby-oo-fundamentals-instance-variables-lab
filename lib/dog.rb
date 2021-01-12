e2953e769bcb3fd8dbbdc10cf37862fc7755ae7f
David Polcari <dpolcari18@gmail.com>
2021-01-11 21:14:43 -0600

    commit tree bcfadb3b577e0fe8815a2b5f20d28fd2405f1714

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Dog
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
+
+    def name
+        @this_dogs_name
+    end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+lassie.name
